6b9c52b2935da597781e9dc16410c3912c29579f
jimmy <jimmy.kroneld@hotmail.com>
2019-12-11 15:18:26 +0100

    small fixes and comments

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -5,12 +5,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* GET chat page. */
 router.get('/chat', function(req, res, next) {
   res.render('chat', { page:'chat',title: 'Express' });
 });
+/* GET register page. */
 router.get('/register', function(req, res, next) {
   res.render('register', { page:'register',title: 'Express' });
 });
+/* GET login page. */
 router.get('/login', function(req, res, next) {
   res.render('login', { page:'log in',title: 'Express' });
 });
