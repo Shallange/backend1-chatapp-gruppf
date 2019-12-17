c5b3d8c1581095d421b6ed6a5d1c778a1838794f
ashurw <ashurw@gmail.com>
2019-12-17 11:17:09 +0100

    fixed register and login

--- chatapp/app.js
+++ chatapp/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var monk = require('monk');
-var db = monk('localhost:3000')
+var db = monk('localhost:27017/fchat')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -5,9 +5,44 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('login');
 });
+
+router.post('/login', function(req, res, next){
+  let db = req.db;
+  let collection = db.get('users');
+
+  collection.findOne({username: req.body.username, password: req.body.password}, function (err, user) {
+    if(err) {
+      console.log(err)
+    } else if(user != null) {
+      console.log(user);
+      res.render('index');
+    } else {
+      console.log(user);
+      res.redirect('/');
+    }
+  })
+});
+
 /* GET register page. */
 router.get('/register', function(req, res, next) {
   res.render('register');
+});
+
+router.post('/register/new', function(req, res, next){
+  let db = req.db;
+  let collection = db.get('users');
+
+  collection.insert({
+    "username": req.body.username,
+    "password": req.body.password
+  }, function (err, data){
+      if(err) {
+        console.log(err);
+      } else {
+        console.log(data);
+      }
+    });
+  res.redirect('/');
 });
 /* GET login page. */
 router.get('/index', function(req, res, next) {
