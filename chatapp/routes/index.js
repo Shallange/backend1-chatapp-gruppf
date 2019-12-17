12dfb25da3b93452820c07544eaf120107719c4a
jimmy <jimmy.kroneld@hotmail.com>
2019-12-17 10:54:46 +0100

    route changes

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -3,15 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('login');
 });
 /* GET register page. */
 router.get('/register', function(req, res, next) {
-  res.render('register', { page:'register',title: 'Express' });
+  res.render('register');
 });
 /* GET login page. */
-router.get('/login', function(req, res, next) {
-  res.render('login', { page:'log in',title: 'Express' });
+router.get('/index', function(req, res, next) {
+  res.render('index');
 });
+
 
 module.exports = router;
